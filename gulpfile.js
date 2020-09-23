f0c74c3b36babdebba04419bf29a4e2d772ad255
asifsh <shahmirghurki@gmail.com>
2020-09-24 02:32:07 +1000

    minor fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 gulp.task('default', function () {
 return gulp.src('src/**/*.+(scss|js)')
-    .pipe(concat('file'))
+    .pipe(concat('file.js'))
     .pipe(gulp.dest('dist'));
 });
